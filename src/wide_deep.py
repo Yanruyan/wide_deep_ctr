a2f8097b3a7fd66ffa4e3ba27efa862658da477b
oucqiangz <605067184@qq.com>
2018-06-18 19:00:53 +0800

    WDL model.

---
+++ src/wide_deep.py
@@ -0,0 +1,15 @@
+####################################################
+# desc : Wide & Deep model for ctr prediction.
+# 
+# author : qiangz2012@yeah.net
+#
+####################################################
+
+
+class WDL(object):
+
+    def __init__( self, ):
+        
+    
+    def _train( self, X_reader, Y_reader ):
+   
